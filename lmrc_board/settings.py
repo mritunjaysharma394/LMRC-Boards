981249bfe2ff56d94b1ab4ca19a01c104752c057
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2019-08-27 21:20:17 +0530

    Changes done in settings.py

--- lmrc_board/settings.py
+++ lmrc_board/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rcqacto6fvia$m+0^yi*r#cp_@(h6pd38c391m=!%0zvq#!23@'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
